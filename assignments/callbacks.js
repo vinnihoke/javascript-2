f3020d3e4e72e4884ec5d82283db23c39f79db2d
Vinni Hoke <vinni.hoke@gmail.com>
2019-05-26 19:10:04 -0700

    Completed Array-Methods

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,49 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+
+getLength(items, (length) => {
+  console.log(length);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr.pop());
 }
+
+last(items, (lastItem) => {
+  console.log(lastItem);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+
+sumNums(3, 5, (add) => {
+  console.log(add);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+
+multiplyNums(4, 5, (multiply) => {
+  console.log(multiply);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
+
+contains(1, [5, 3, 2, 1, 6], (contains) => {
+  console.log(contains);
+});
 
 /* STRETCH PROBLEM */
 
@@ -52,4 +77,6 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+
 }
